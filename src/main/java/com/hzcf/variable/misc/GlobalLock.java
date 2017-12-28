bbaae5019dfa197d1c405fcea9164c5b7b2a43f0
huhl <huhl@gitlab.hzbd.com>
2017-12-28 16:28:13 +0800

    补传全局锁

---
+++ src/main/java/com/hzcf/variable/misc/GlobalLock.java
@@ -0,0 +1,11 @@
+package com.hzcf.variable.misc;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class GlobalLock {
+    private static ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock(true);
+
+    public static ReentrantReadWriteLock.ReadLock readLock = reentrantReadWriteLock.readLock();
+
+    public static ReentrantReadWriteLock.WriteLock writeLock = reentrantReadWriteLock.writeLock();
+}
